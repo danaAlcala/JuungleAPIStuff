43f43214473fd09bcd2d5b3d15aa7f9e249f081b
danaAlcala <bigdalcala@gmail.com>
2021-04-02 23:32:25 -0700

    Having a go at the API
    Just trying different things to get used to manipulating data from the API.  Soon I need to learn how to log in and change prices with the API.

---
+++ LearnJuungleAPI/code.js
@@ -0,0 +1,46 @@
+const BCHSATOSHIS = 100000000;
+
+var receivedJSONdata;
+var NFTs;
+var tokenID = "88dac34fd486cdca5d011c0eed7cae661ebc1bd16df949a6b6e22226de44a024";
+var jsonurl = "https://www.juungle.net/api/v1/nfts?groupTokenId=" + tokenID + "&priceSatoshisSet=true&purchaseTxidUnset=true";
+var soldTokens = [];
+var BCHtoUSD = 0;
+
+function satoshisToBCH(paramSatoshis)
+{
+    return paramSatoshis / BCHSATOSHIS;
+}
+
+function BCHtoUSDollars(paramBCH)
+{
+    return paramBCH * BCHtoUSD;
+}
+
+function getData(paramURL)
+{
+    $.getJSON("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin-cash&vs_currencies=usd", function(data)
+    {
+        BCHtoUSD = data;
+        console.log(BCHtoUSD);
+    })
+    /*$.getJSON(paramURL,function(data)
+    {
+        receivedJSONdata = data;
+        NFTs = receivedJSONdata.nfts;
+        let i;
+        for (i=0; i< NFTs.length; i++)
+        {
+            let tokenName = NFTs[i].tokenName;
+            let tokenPrice = satoshisToBCH(NFTs[i].priceSatoshis).toString();
+            let tokenUSD = BCHtoUSDollars(tokenPrice);
+            console.log(tokenName + ": " + tokenPrice + " BCH $" + tokenUSD);
+            if (NFTs[i].userId !== 296)
+            {
+                soldTokens.push(NFTs[i]);
+            }
+        }
+        console.log(soldTokens);
+    }
+    );*/
+}
