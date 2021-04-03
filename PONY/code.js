99ca4bc97a57030b1248d2cfc966c60a91c8ed1e
danaAlcala <bigdalcala@gmail.com>
2021-04-02 23:27:08 -0700

    Pony
    I was trying to price some ponies I was trying to flip. I almost made my money back. At about $1 loss, I have 7 left on the marketplace.

---
+++ PONY/code.js
@@ -0,0 +1,52 @@
+var numberOfPONYSold = PONY.length;
+
+function totalPONYcost()
+{
+    let i;
+    let totalSatoshis = 0;
+    for (i=0; i < numberOfPONYSold; i++)
+    {
+        totalSatoshis += PONY[i].priceSatoshis;
+    }
+    return totalSatoshis;
+}
+function medianPONYcost()
+{
+    let newArray = [];
+    let i;
+    for (i=0; i < numberOfPONYSold; i++)
+    {
+        newArray.push(PONY[i].priceSatoshis);
+    }
+    newArray.sort((a, b) => a - b);
+    for (i=0; i < newArray.length; i++)
+    {
+        console.log(newArray[i]);
+    }
+    if (newArray.length % 2 === 0)
+    {
+        let halfIndex = newArray.length / 2;
+        return (newArray[halfIndex] + newArray[halfIndex + 1]) / 2;
+    }
+    else
+    {
+        let halfIndex = newArray.length / 2;
+        return newArray[Math.floor(halfIndex) + 1];
+    }
+}
+function printAllPrices()
+{
+    let i;
+    for (i=0; i < numberOfPONYSold; i++)
+    {
+        console.log(PONY[i].priceSatoshis);
+    }
+}
+
+var averageSatoshiPerSale = totalPONYcost() / numberOfPONYSold;
+
+console.log(numberOfPONYSold + " PONY have sold to date");
+console.log(totalPONYcost() + " satoshis have been earned so far.");
+console.log("The average price per PONY has so far been " + averageSatoshiPerSale.toString() + " satoshis.");
+console.log("The median price per PONY has so far been " + medianPONYcost() + " satoshis.");
+//printAllPrices();
