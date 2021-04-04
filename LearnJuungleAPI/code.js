37857c742062a17e7969e02827b7bd1b2b7d55f9
danaAlcala <bigdalcala@gmail.com>
2021-04-04 03:49:30 -0700

    Drawing up a plan
    I will want to display everything in a table. Coming soon.

--- LearnJuungleAPI/code.js
+++ LearnJuungleAPI/code.js
@@ -1,10 +1,21 @@
 const BCHSATOSHIS = 100000000;
+
+//API URL Params for NFTs
+const AVAILABLE = "&priceSatoshisSet=true";
+const NOTPURCHASED = "&purchaseTxidUnset=true";
+
 const COINGECKO = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin-cash&vs_currencies=usd";
 
+var tokenIDs =
+{
+    "PHOTOYSHOP": "88dac34fd486cdca5d011c0eed7cae661ebc1bd16df949a6b6e22226de44a024",
+    "JLV": "0ece7b7a27bd62bf2abecede838321cb65842cbb0ae1398d51d77d264338698d",
+    "lucivay": "454f7411a1a5a1af8e310b478fa8deae24fa4d321c499a11229c16085d1b0553"
+};
 var receivedJSONdata;
 var NFTs;
-var tokenID = "88dac34fd486cdca5d011c0eed7cae661ebc1bd16df949a6b6e22226de44a024";
-var jsonurl = "https://www.juungle.net/api/v1/nfts?groupTokenId=" + tokenID + "&priceSatoshisSet=true&purchaseTxidUnset=true";
+var tokenID = tokenIDs.PHOTOYSHOP;
+var jsonurl = "https://www.juungle.net/api/v1/nfts?groupTokenId=" + tokenID + AVAILABLE + NOTPURCHASED;
 var soldTokens = [];
 var BCHtoUSD = 0;
 
@@ -24,9 +35,13 @@ function getUSDPrice()
     {
         let BCHtoUSDdata = data;
         let strBCHtoUSD = JSON.stringify(BCHtoUSDdata)
+        let dataButton = document.getElementById("Data");
+        let bigMoney = document.getElementById("USD");
         console.log(strBCHtoUSD);
         console.log(BCHtoUSDdata["bitcoin-cash"]["usd"]);
         BCHtoUSD = BCHtoUSDdata["bitcoin-cash"]["usd"]
+        bigMoney.innerHTML = "1 BCH = $" + BCHtoUSD.toString();
+        dataButton.disabled = false;
     })
 }
 
@@ -55,8 +70,14 @@ function getNFTData(paramURL)
 
 function getData(paramURL)
 {
-    getUSDPrice();
     getNFTData(paramURL);    
 }
 
-//getData(jsonurl);
+function buildTable()
+{    
+    let rows = 0;
+    let columns = 0;
+    var table = document.getElementById("myTable");
+    table.insertRow(1);
+    table[1][0].innerHTML = "Hello";
+}
